bc1bddf12331ac5b1e0a578cfc983a888e75b328
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-08-08 14:50:38 +0100

    Add service to handle fetching data from upstream API.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require( 'path' );
+
+const NodemonPlugin = require( 'nodemon-webpack-plugin' );
+
+module.exports = {
+	mode: 'development',
+	entry: './src/app.js',
+	target: 'node',
+	output: {
+		filename: 'main.js',
+		path: path.resolve( __dirname, 'dist' ),
+	},
+	optimization: {
+		minimize: false,
+	},
+	plugins: [
+		new NodemonPlugin(),
+	],
+};
