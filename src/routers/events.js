855bedbce06f53a6c1875fa74836327822892e71
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-08-09 13:33:26 +0100

    Process data.

--- src/routers/events.js
+++ src/routers/events.js
@@ -1,19 +1,45 @@
 /**
- * Endpoint for `/events`.
+ * Endpoint to deal with events.
  */
 
 const express = require( 'express' );
 
+const DataService = require( '../services/data' );
+
 const router = express.Router();
 
-router.get( '/', ( req, res ) => {
+router.get( '/', async ( req, res ) => {
 	const { lang, sportId } = req.query;
 
-	res.json( {
-		lang,
-		sportId,
-		type: 'events',
-	} );
+	try {
+		const upstreamData = await DataService.fetch( lang );
+
+		const nSportId = parseInt( sportId );
+
+		const data = upstreamData.sports
+			.filter( sport => ( sportId == null ) || ( nSportId === sport.id ) )
+			.flatMap( sport => sport.comp )
+			.flatMap( comp => comp.events )
+			.map( event => ( {
+				id: event.id,
+				pos: event.pos,
+				desc: event.desc,
+			} ) )
+//			.sort( ( e1, e2 ) => e1.pos - e2.pos ) TODO: sort by grouping?
+		;
+
+		res.json( {
+			status: 'success',
+			data,
+		} );
+	} catch ( e ) {
+		console.log( 'Error accessing data @events', e );
+
+		res.status( 500 ).json( {
+			status: 'error',
+			error: e.message,
+		} );
+	}
 } );
 
 router.get( '/:id', ( req, res ) => {
