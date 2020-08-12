52585f57f5e3aac27d29b7775ace586ef08cde9a
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-08-12 18:10:28 +0100

    Test with sport ID.

--- test/routers/events.test.js
+++ test/routers/events.test.js
@@ -50,7 +50,7 @@ const mockResponseDe = {
 						'pos': 14,
 						'events': [
 							{
-								'id': 12,
+								'id': 13,
 								'desc': 'FC1 vs. FC2',
 								'pos': 9999,
 							},
@@ -67,8 +67,10 @@ describe( 'events router', () => {
 	describe( 'GET /events', () => {
 
 		test( 'with upstream error', async () => {
+			const lang = 'en-gb';
+
 			nock( 'https://partners.betvictor.mobi' )
-				.get( '/en-gb/in-play/1/events' )
+				.get( `/${lang}/in-play/1/events` )
 				.reply( 200, {
 					'status': {
 						'success': false,
@@ -77,7 +79,7 @@ describe( 'events router', () => {
 				} )
 			;
 
-			const res = await request( app ).get( '/events?lang=en-gb' );
+			const res = await request( app ).get( `/events?lang=${lang}` );
 
 			expect( res.status ).toBe( 500 );
 
@@ -103,7 +105,7 @@ describe( 'events router', () => {
 			expect( res.body.data[ 0 ] ).toEqual( mockResponseEn.result.sports[ 0 ].comp[ 0 ].events[ 0 ] );
 		} );
 
-		test( 'using German language', async done => {
+		test( 'using German language', async () => {
 			const lang = 'de-de';
 
 			nock( 'https://partners.betvictor.mobi' )
@@ -120,6 +122,27 @@ describe( 'events router', () => {
 			expect( res.body.data.length ).toBe( mockResponseDe.result.total_number_of_events );
 
 			expect( res.body.data[ 0 ] ).toEqual( mockResponseDe.result.sports[ 0 ].comp[ 0 ].events[ 0 ] );
+		} );
+
+		test( 'with sportId', async done => {
+			const lang = 'de-de';
+
+			const sportId = mockResponseDe.result.sports[ 0 ].id;
+
+			const mockEvent = mockResponseDe.result.sports[ 0 ].comp[ 0 ].events[ 0 ];
+
+			nock( 'https://partners.betvictor.mobi' )
+				.get( `/${lang}/in-play/1/events` )
+				.reply( 200, mockResponseDe )
+			;
+
+			const res = await request( app ).get( `/events?lang=${lang}&sportId=${sportId}` );
+
+			expect( res.status ).toBe( 200 );
+
+			expect( res.body.status ).toEqual( 'success' );
+
+			expect( res.body.data.length ).not.toBe( 0 );
 
 			done();
 		} );
