e6158031b6d388556b7c4f1aa5a207ed40d0a5a0
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-08-12 23:31:49 +0100

    Use handler to deal with errors and repsonses in the controller.

--- src/routers/events.js
+++ src/routers/events.js
@@ -4,57 +4,24 @@
 
 const express = require( 'express' );
 
+const { makeController } = require( '../util' );
+
 const EventsModel = require( '../models/events' );
 
 const router = express.Router();
 
-router.get( '/', async ( req, res ) => {
+router.get( '/', makeController( ( req, res ) => {
 	const { lang, sportId } = req.query;
 
-	try {
-		const data = await EventsModel.getEvents( lang, sportId );
+	return EventsModel.getEvents( lang, sportId );
+} ) );
 
-		res.json( {
-			status: 'success',
-			data,
-		} );
-	} catch ( e ) {
-		console.log( 'Error accessing data @events', e );
-
-		res.status( 500 ).json( {
-			status: 'error',
-			error: e.message,
-		} );
-	}
-} );
-
-router.get( '/:id', async ( req, res ) => {
+router.get( '/:id', makeController( ( req, res ) => {
 	const { id } = req.params;
 
 	const { lang } = req.query;
 
-	try {
-		const data = await EventsModel.getEvent( lang, id );
-
-		if ( data ) {
-			res.json( {
-				status: 'success',
-				data,
-			} );
-		} else {
-			res.status( 404 ).json( {
-				status: 'error',
-				error: `Event with ID ${id} not found.`,
-			} );
-		}
-	} catch ( e ) {
-		console.log( 'Error accessing data @events/:id', e );
-
-		res.status( 500 ).json( {
-			status: 'error',
-			error: e.message,
-		} );
-	}
-} );
+	return EventsModel.getEvent( lang, id );
+} ) );
 
 module.exports = router;
